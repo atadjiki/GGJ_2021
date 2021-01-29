c831b70d195a6c0cfad0d227db398794f4570ced
andrewtaylorjohnson <andrewtaylorjohnson@gmail.com>
2021-01-29 13:50:07 -0700

    Added props to engine.

---
+++ LostAndFound/Assets/Pixelate Image Effect/Pixelate Image Effect URP/Scripts/PixelateImageEffect.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class PixelateImageEffect : ScriptableRendererFeature
+{
+    [System.Serializable]
+    public class PixelateImageEffectSettings
+    {
+        public RenderPassEvent renderPassEvent = RenderPassEvent.AfterRenderingTransparents;
+
+        [Range(1, 50)]
+        public int tileSize = 10;
+
+        public Material material = null;
+    }
+
+    public PixelateImageEffectSettings settings = new PixelateImageEffectSettings();
+
+    class CustomRenderPass : ScriptableRenderPass
+    {   
+        string profilerTag;
+
+        private int sourceId_copy;
+        private RenderTargetIdentifier sourceRT_copy;
+        public PixelateImageEffectSettings settings = null;
+        
+        private RenderTargetIdentifier source { get; set; }
+
+        public void Setup(RenderTargetIdentifier source) {
+            this.source = source;
+        }
+
+        public CustomRenderPass(string profilerTag)
+        {
+            this.profilerTag = profilerTag;
+        }
+
+        public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor)
+        {
+            var width = cameraTextureDescriptor.width;
+            var height = cameraTextureDescriptor.height;
+
+            sourceId_copy = Shader.PropertyToID("_SourceRT_Copy");
+            cmd.GetTemporaryRT(sourceId_copy, width, height, 0, FilterMode.Bilinear, cameraTextureDescriptor.colorFormat);
+
+            sourceRT_copy = new RenderTargetIdentifier(sourceId_copy);
+            
+            ConfigureTarget(sourceRT_copy);
+        }
+
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            CommandBuffer cmd = CommandBufferPool.Get(profilerTag);
+
+            RenderTextureDescriptor opaqueDesc = renderingData.cameraData.cameraTargetDescriptor;
+            opaqueDesc.depthBufferBits = 0;
+
+            cmd.Blit(source, sourceRT_copy);
+
+            cmd.SetGlobalFloat("_TileSize", settings.tileSize);
+            cmd.Blit(sourceRT_copy, source, settings.material, 0);
+
+            context.ExecuteCommandBuffer(cmd);
+            cmd.Clear();
+
+            CommandBufferPool.Release(cmd);
+        }
+
+        public override void FrameCleanup(CommandBuffer cmd)
+        {
+        }
+    }
+
+    CustomRenderPass scriptablePass;
+
+    public override void Create()
+    {
+        scriptablePass = new CustomRenderPass("PixelateImageEffect");
+        scriptablePass.settings = settings;
+
+        scriptablePass.renderPassEvent = settings.renderPassEvent;
+    }
+
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        var src = renderer.cameraColorTarget;
+        scriptablePass.Setup(src);
+        renderer.EnqueuePass(scriptablePass);
+    }
+}
+
+
---
+++ LostAndFound/Assets/Pixelate Image Effect/Pixelate Image Effect URP/Shaders/PixelateImageEffect.shader
@@ -0,0 +1,71 @@
+﻿Shader "Hidden/PixelateImageEffect"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+	SubShader
+	{
+		Cull Off ZWrite Off ZTest Always
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			#pragma target 3.0
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+			};
+
+			struct v2f
+			{
+				float4 pos : SV_POSITION;
+				float4 scrPos : TEXCOORD0;
+			};
+
+			sampler2D _MainTex;
+			float4 _MainTex_TexelSize;
+
+			uniform half _TileSize;
+
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.pos = UnityObjectToClipPos(v.vertex);
+				o.scrPos = ComputeScreenPos(o.pos);
+				return o;
+			}
+
+			float2 fmod(float2 a, float2 b)
+			{
+				float2 c = frac(abs(a / b)) * abs(b);
+				return abs(c);
+			}
+
+			fixed4 frag (v2f i) : SV_Target
+			{
+				float2 fragCoord = (i.scrPos.xy/i.scrPos.w) * _ScreenParams.xy;
+				float4 c = 0;
+				
+				float2 uv = fragCoord.xy / _ScreenParams.xy;
+				float2 size = 1.0 / float2(_ScreenParams.x, _ScreenParams.y) * _TileSize;
+
+				float2 Pbase = uv - fmod(uv, size);
+				float2 PCenter = Pbase + size / 2.0;
+				float2 st = (uv - Pbase) / size;
+
+				float4 tileColor = tex2D(_MainTex, PCenter);
+				c = tileColor;
+
+				return c;
+			}
+
+			ENDCG
+		}
+	}
+}
