2c642831d5feb57550024e6589bb516729216cae
Arash Tadjiki <atadjiki@gmail.com>
2021-01-30 14:40:02 -0700

    Really rough controls

---
+++ LostAndFound/Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public RawImage crosshair;
+
+    private static UIManager _instance;
+
+    public static UIManager Instance { get { return _instance; } }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    private void ToggleCrosshair(bool flag)
+    {
+        crosshair.enabled = flag;
+    }
+
+    public void SetDefault()
+    {
+        crosshair.color = Color.white;
+    }
+
+    public void SetObjectInRange()
+    {
+        crosshair.color = Color.green;
+    }
+
+    public void SetObjectGrabbed()
+    {
+        ToggleCrosshair(false);
+    }
+
+    public void SetObjectReleased()
+    {
+        ToggleCrosshair(true);
+    }
+
+}
---
+++ LostAndFound/Assets/Scripts/Player/StickyArm.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickyArm : MonoBehaviour
+{
+    [SerializeField] public float Range = 50;
+
+    private Interactable AttachedTo = null;
+
+    private LineRenderer ArmRenderer;
+    public int ArmDensity = 2;//how many points to have in the arm
+
+    private LineOrigin originObject;
+    
+    public enum State { Empty, AttachedToSurface, AttachedToItem };
+    public State CurrentState = State.Empty;
+
+    private void Awake()
+    {
+        ArmRenderer = GetComponent<LineRenderer>();
+        originObject = GetComponentInChildren<LineOrigin>();
+
+        ArmRenderer.widthMultiplier = 0.2f;
+        ArmRenderer.positionCount = ArmDensity;
+    }
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if(IsAttached())
+        {
+            UpdateArmRenderer();
+        }
+    }
+
+    public bool IsAttached()
+    {
+        return (AttachedTo != null);
+    }
+
+    public Interactable GetAttachedTo()
+    {
+        if(IsAttached())
+        {
+            return AttachedTo;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public void RegisterInteractable(Interactable newObject)
+    {
+        AttachedTo = newObject;
+        ArmRenderer.enabled = true;
+        UpdateArmRenderer();
+    }
+
+    public void GrabObject(ItemObject itemObject)
+    {
+        AttachedTo = itemObject;
+        ArmRenderer.enabled = true;
+        UpdateArmRenderer();
+
+        CurrentState = State.AttachedToItem;
+    }
+
+    public void GrabSurface(GrappleObject grappleObject)
+    {
+        AttachedTo = grappleObject;
+        ArmRenderer.enabled = true;
+        UpdateArmRenderer();
+
+        CurrentState = State.AttachedToSurface;
+    }
+
+    public void Release()
+    {
+        AttachedTo = null;
+        ArmRenderer.enabled = false;
+
+        CurrentState = State.Empty;
+    }
+
+    private void UpdateArmRenderer()
+    {
+        Vector3[] points = new Vector3[ArmDensity];
+        points[0] = originObject.transform.position;
+        points[points.Length-1] = AttachedTo.transform.position;
+
+        ArmRenderer.SetPositions(points);
+    }
+
+    public Vector3 GetArmOrigin()
+    {
+        return originObject.transform.position;
+    }
+}
