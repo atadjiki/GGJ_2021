f451acf18c08834721911f99fbd0e54e4526f9fb
andrewtaylorjohnson <andrewtaylorjohnson@gmail.com>
2021-01-30 23:32:07 -0700

    Prefabbed all current props. Added script to give items an initial force in zero-g.

--- LostAndFound/Assets/Scripts/Objects/ItemObject.cs
+++ LostAndFound/Assets/Scripts/Objects/ItemObject.cs
@@ -4,5 +4,16 @@ using UnityEngine;
 
 public class ItemObject : Interactable
 {
+    private Rigidbody _rb;
 
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        Vector3 randomDirection = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+        float randomForce = Random.Range(0.25f, 1f);
+        _rb.AddForce(randomDirection * randomForce, ForceMode.Impulse);
+        Vector3 randomRotation = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+        float randomRotationForce = Random.Range(0.1f, 0.25f);
+        _rb.AddTorque(randomRotation * randomRotationForce, ForceMode.Impulse);
+    }
 }
